285eb137476fdf302fdaddaeb729d118e0f88772
Austin <77835617+AustinBoath@users.noreply.github.com>
2021-08-17 17:05:18 +0930

    added the python

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Tux: Hello! Welcome to the Lemonator, a lemonade stand. We're business partners, and we're going to dominate the lemonade industry. Let's begin!")
+print("WHAT DO YOU SAY?")
+print("1. Why lemonade? That won't make us much money.")
+print("2. Let's do this!")
+firstmotivation = input("Enter 1-2 to speak the dialogue:  ")
+if firstmotivation is 1:
+    print("You: Why lemonade? That won't make us much money.")
+    print("Tux is angry and leaves to go to Antarctica. He thinks maybe trying to enter a capitalist society was dumb. He would rather go home and catch fish.");
+if firstmotivation is 2:
+    print("You: Let's do this!")
+    print("Tux: Alright then. Time for a big business decision! Cups are free thanks to our sponsor, and we steal water like Nestle. However, Farmer Joe wants $1 per lemon. It will take anywhere from 2-4 lemons per cup depending on their quality.")
+    lemons = input("Tux invested $20. How many lemons will you buy? Note: You will need money to repair your lemonade stand if it is damaged.");
+if firstmotivation > 2:
+    firstmotivation=input("Invalid input. Please enter 1 or 2: ");
+    if firstmotivation > 1:
+        firstmotivation=input("Invalid input. Please enter 1 or 2: ");
+    if lemons = >=< 0:
+        print("what a shame. You are only using a free trial of this game. You will need to buy the full version. Wait, this is GNU GPL software. Never mind. The developer is just too lazy to finish the game.")
